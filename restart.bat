d19dd0b28efae408570a944dfff04968f918cf15
andsim <andsim@ansky.ca>
2024-09-16 14:38:09 -0400

    this file send message and restart
    this file run in windows only. this file can be modify for linux because i dont have linux so i cant test in .sh

---
+++ restart.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+REM Run broadcast.bat three times with a 1-minute interval
+for /L %%i in (1,1,3) do (
+    call broadcast.bat server is shutting down due to schedule restart please wait 10 mins. thank you
+    timeout /t 60 /nobreak
+)
+
+REM Wait for the remaining 2 minutes
+timeout /t 5 /nobreak
+
+REM Run down.bat
+call down.bat
+timeout /t 5 /nobreak
+call up.bat
+endlocal
